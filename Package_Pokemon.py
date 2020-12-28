44e4a68bf034b683576edbd7bfb8dec99831872b
denvoros <25395758+denvoros@users.noreply.github.com>
2020-12-28 14:19:58 -0700

    autopep Package_Pokemon.py

--- Package_Pokemon.py
+++ Package_Pokemon.py
@@ -7,7 +7,8 @@ from Pokemon_Data.Pokemon import Pokemon
 from Pokemon_Data.Move import Move
 from Pokemon_Data import matchup_scoring
 from copy import copy
-import csv, pickle
+import csv
+import pickle
 
 spread_move_list = []
 with open('Pokemon_Data/Spread_moves.txt', newline='\n') as tsvfile:
@@ -18,7 +19,7 @@ print('Read and processed spread move file.')
 
 move_list = {}
 with open('Pokemon_Data/Moves.csv', newline='\n') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar = '"')
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
         Name = row[0]
         Type = row[1].title()
@@ -52,21 +53,24 @@ with open('Pokemon_Data/Moves.csv', newline='\n') as csvfile:
             multiplier *= 2.2575
         elif 'Attacks thrice with more power each time.' in Effect:
             multiplier *= 94.14/20/Accuracy
-        move_list[Name] = Move(Name, Type, Category, Power, Accuracy, PP, TM, Effect, Probability, is_spread=(Name in spread_move_list), correction_factor=multiplier)
+        move_list[Name] = Move(Name, Type, Category, Power, Accuracy, PP, TM, Effect, Probability, is_spread=(
+            Name in spread_move_list), correction_factor=multiplier)
 print('Read and processed move file.')
 
 max_move_list = {}
 status_max_move = None
 with open('Pokemon_Data/Max_moves.txt', newline='\n') as tsvfile:
-    spamreader = csv.reader(tsvfile, delimiter='\t', quotechar = '"')
+    spamreader = csv.reader(tsvfile, delimiter='\t', quotechar='"')
     for row in spamreader:
         Name = row[0]
         Type = row[1].title()
         Effect = row[2]
         if Name != 'Max Guard':
-            max_move_list[Type] = Move(Name, Type, 0, 0, 1, None, None, Effect, 100)
+            max_move_list[Type] = Move(
+                Name, Type, 0, 0, 1, None, None, Effect, 100)
         else:
-            status_max_move = Move(Name, Type, 'Status', 0, 1, None, None, Effect, 100)
+            status_max_move = Move(Name, Type, 'Status',
+                                   0, 1, None, None, Effect, 100)
 print('Read and processed max move file.')
 
 pokemon_base_stats = {}
@@ -74,7 +78,8 @@ with open('Pokemon_Data/All_Pokemon_stats.txt', newline='\n') as tsvfile:
     spamreader = csv.reader(tsvfile, delimiter='\t', quotechar='"')
     for row in spamreader:
         Name = row[2]
-        stats = (int(row[3]), int(row[4]), int(row[5]), int(row[6]), int(row[7]), int(row[8]))
+        stats = (int(row[3]), int(row[4]), int(row[5]),
+                 int(row[6]), int(row[7]), int(row[8]))
         pokemon_base_stats[Name] = stats
 print('Read and processed Pokemon stats file.')
 
@@ -90,7 +95,7 @@ print('Read and processed Pokemon types file.')
 rental_pokemon = {}
 with open('Pokemon_Data/Rental_Pokemon.txt', newline='\n') as file:
     spamreader = csv.reader(file)
-    i=0
+    i = 0
     dump = []
     for row in spamreader:
         dump.append(row)
@@ -126,13 +131,14 @@ with open('Pokemon_Data/Rental_Pokemon.txt', newline='\n') as file:
         counter = 2
         if name in rental_pokemon:
             print('WARNING: Duplicate entry: '+name)
-        rental_pokemon[name] = Pokemon(name,ability,pokemon_types[name],pokemon_base_stats[name],moves,max_moves,level)
+        rental_pokemon[name] = Pokemon(
+            name, ability, pokemon_types[name], pokemon_base_stats[name], moves, max_moves, level)
 print('Read and processed rental Pokemon file.')
 
 boss_pokemon = {}
 with open('Pokemon_Data/Boss_Pokemon.txt', newline='\n') as file:
     spamreader = csv.reader(file)
-    i=0
+    i = 0
     dump = []
     for row in spamreader:
         dump.append(row)
@@ -163,14 +169,16 @@ with open('Pokemon_Data/Boss_Pokemon.txt', newline='\n') as file:
             max_move.PP = move.PP
             max_moves.append(max_move)
             i += 1
-        boss_pokemon[name] = Pokemon(name,ability,pokemon_types[name],pokemon_base_stats[name],moves,max_moves,level)
+        boss_pokemon[name] = Pokemon(
+            name, ability, pokemon_types[name], pokemon_base_stats[name], moves, max_moves, level)
 print('Read and processed boss Pokemon file.')
 
 boss_matchup_LUT = {}
 for key, attacker in rental_pokemon.items():
     matchups = {}
     for key, defender in boss_pokemon.items():
-        matchups[defender.name] = matchup_scoring.evaluate_matchup(attacker, defender, rental_pokemon)
+        matchups[defender.name] = matchup_scoring.evaluate_matchup(
+            attacker, defender, rental_pokemon)
     boss_matchup_LUT[attacker.name] = matchups
     print('Finished computing matchups for '+str(attacker))
 print('Computed boss matchup LUT.')
@@ -182,7 +190,8 @@ for key, attacker in rental_pokemon.items():
     matchups = {}
     attacker_score = 0
     for key, defender in rental_pokemon.items():
-        matchups[defender.name] = matchup_scoring.evaluate_matchup(attacker, defender, rental_pokemon)
+        matchups[defender.name] = matchup_scoring.evaluate_matchup(
+            attacker, defender, rental_pokemon)
         attacker_score += matchups[defender.name]
     rental_matchup_LUT[attacker.name] = matchups
     rental_pokemon_scores[attacker.name] = attacker_score
@@ -192,8 +201,7 @@ for key, attacker in rental_pokemon.items():
 for key in rental_pokemon_scores:
     rental_pokemon_scores[key] /= (total_score/len(rental_pokemon))
 print('Computed rental matchup LUT.')
-    
-    
+
 
 with open('Pokemon_Data/Rental_Pokemon.pickle', 'wb') as file:
     pickle.dump(rental_pokemon, file)
@@ -206,4 +214,3 @@ with open('Pokemon_Data/Rental_Matchup_LUT.pickle', 'wb') as file:
 with open('Pokemon_Data/Rental_Pokemon_Scores.pickle', 'wb') as file:
     pickle.dump(rental_pokemon_scores, file)
 print('Finished packaging Pokemon!')
-
